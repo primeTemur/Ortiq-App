39eea4a52a8c4c6515c58de904b2a7b1d8b2aa00
Temur <temuru502@gmail.com>
2025-12-01 13:23:16 +0500

    test II

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    'https://calc-app-beta-two.vercel.app',
 ]
 
 # Application definition
@@ -89,6 +90,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://calc-app-beta-two.vercel.app',
 ]
 
 
